5bf05f778eb2eef9a265342cac8958ed3916e48b
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-10-16 22:28:49 -0400

    Exercise 5

---
+++ exercise5.py
@@ -0,0 +1,17 @@
+my_name = "Davian Perez-Silva"
+my_age = 21
+my_height = 72
+my_weight = 180 #pounds
+my_eyes = "brown"
+my_teeth = "white"
+my_hair = "brown"
+
+print("Lets talk about %s." % my_name)
+print("He's %d inches tall." % my_height)
+print("He's %d pounds heavy." % my_weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair" % (my_eyes, my_hair))
+print("His teeth are usually %s depending on the coffee" % my_teeth)
+print("If I add %d, %d and %d I get %d." % (my_age, my_height, my_weight, (my_age + my_height + my_weight)))
+
+# %d, %s, %r are formatters. Reminds me of variables within the function
