9075365c9b68dc6597e9469a9721d1681ee500e9
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-10-17 22:45:36 -0400

    no message

--- Exercises/exercise7.py
+++ Exercises/exercise7.py
@@ -16,5 +16,4 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that comma at the end. try removing it to see what happens
 print(end1 + end2 + end3 + end4 + end5 + end6, end7 + end8 + end9 + end10 + end11 + end12)
