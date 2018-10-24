6ae2481b2be4fbae0537cbaa56e80dc135991ee5
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-10-23 23:20:45 -0400

    exercise 12

--- Exercises/exercise11.py
+++ Exercises/exercise11.py
@@ -1,9 +1,9 @@
-print("How old are you?"),
-age = input()
+print("How old are you?"), # Prints
+age = input() #Input your age manually
 print("How tall are you?"),
 height = input()
 print("How much do you weigh?"),
 weight = input()
 
 print("So , you are %r old, %r tall and %r heavy."
-% (age, height, weight))
+% (age, height, weight)) # %r is like a variable, (age, height, weight) will define it 
